7d5af677938fe71f201a199f7388ffc23b17f8b9
unknown <abdiaziizarrale@gmail.com>
2021-12-04 04:16:02 -0800

    my Firt UoH project

---
+++ test.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+    let activeSlide = document.querySelector('.slide.translate-x-0');
+    activeSlide.classList.remove('translate-x-0');
+    activeSlide.classList.add('-translate-x-full');
+    
+    let nextSlide = activeSlide.nextElementSibling;
+    nextSlide.classList.remove('translate-x-full');
+    nextSlide.classList.add('translate-x-0');
+
+
+}, 3000);
